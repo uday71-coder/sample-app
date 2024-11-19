279f3501c7659012a7e573541facdb6b30b64501
uday71-coder <bajaruudaysai@gmail.com>
2024-11-19 10:53:28 +0530

    Add files via upload

---
+++ OnlineTest.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+class OnlineTest extends Jframe implements ActionListener{
+Jlabel 1;
+jRadioButton jb[]=new JRadioButton[5];
+JButton b1,b2;
+ButtonGroup bg;
+int count=0,current=0,x=1,y=1,now=0;
+int m[
